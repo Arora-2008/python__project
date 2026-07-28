0e4f0e475bd88ba954bd72b43ec251caa979149f
Geetikaarora-2008 <geetikaarora.arora2008@gmail.com>
2026-07-28 12:13:19 +0530

    s

---
+++ info.py
@@ -0,0 +1,44 @@
+USER_INFO={
+    "name":"geetika",
+    "age":18,
+    "course":"b.tech in ai&ml",
+    "address":"garhshankar",
+
+    "interests":[
+        "Artificial intelligence",
+        "Machine learning",
+        "cybersecurity",
+        "python",
+        "IOT"
+    ],
+
+    "personality_traits":[
+        "creative",
+        "easily adaptable",
+        "easily bored",
+        "fun-loving",
+        "curios"
+    ],
+
+    "background":"""
+    I am a student who has recently completed her 12th grade in science stream with main
+    subjects physics,chemistry and maths. now I am going to persue b.tech in specialisation 
+    with artificial intelligence and machine learning. now I am learning python and git 
+    to enhance my skills.  
+        """,
+
+    "RESPONSE_TONE":{
+        "primary_tone":"friendly and humorous",
+        "formality_level":"casual"
+    },
+
+    "RESPONSE_STYLE":{
+        "response_length":"short",
+        "use_emojis":True,
+        "special_instructions":"""
+         give answer professionally with english as main language .
+         give answer with full respect and fun.
+         give answer with examples in programming.   
+         """
+    }
+    }
